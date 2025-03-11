f7aece5629a9aadd9bca2bbbcc966195954c353b
Anthony Herrera <anthony.herrera@oracle.com>
2025-03-11 16:55:03 -0400

    Updated program to include comments.

--- myscript.js
+++ myscript.js
@@ -2,81 +2,89 @@
 
 //generate a number between 0 and 2
 //0 = Rock, 1= Paper, 2=Scissors
-function getComputerChoice(){
-    return(Math.floor((Math.random() * 3)));
+//Return the string in UpperCase.
+computerSelection = function getComputerChoice(){
+    switch(Math.floor((Math.random() * 3))){
+        case 0:
+            return "ROCK";
+            break;
+        case 1:
+            return "PAPER";
+            break;
+        case 2:
+            return "SCISSORS";
+            break;
+    }
 }
-getComputerChoice();
 
-function getHumanChoice(){
-    return(prompt("Enter the word Rock, Paper, or Scissors"));
+//Get Rock, Paper, or Scissor selection from Human. Return the value in Uppercase if the value is valid.
+humanSelection = function getHumanChoice(){
+    keepGoing = true;
+    while (keepGoing){
+        userInput = prompt("Enter the word Rock, Paper, or Scissors");
+        if (userInput !== null && (userInput.toUpperCase() == "ROCK" || userInput.toUpperCase() == "PAPER" || userInput.toUpperCase() == "SCISSORS")){
+            return userInput.toUpperCase();
+        }
+    }
 }
-
-
-
-//const humanSelection = getHumanChoice();
-//const computerSelection = getComputerChoice();
 
 function playGame(){
     let humanScore = 0;
     let computerScore = 0;
     function playRound(humanChoice, computerChoice){
-        switch(humanChoice.toUpperCase()){
-            case "ROCK":
-                switch (computerChoice){
-                    case 0:
-                        console.log("Tie! You both picked Rock.");
-                        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
-                        break;
-                    case 1:
-                        console.log("You lose! Paper beats Rock!");
-                        computerScore++;
-                        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
-                        break;
-                    default:
-                        console.log("You win! Rock beats Scissors!");
-                        humanScore++;
-                        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+        console.log(`You picked ${humanChoice} and the computer picked ${computerChoice}`);
+        //If its a tie, then skip all the checking to see who wins.
+        if (humanChoice === computerChoice){
+            console.log("TIE!");
+        }
+        //check to see who wins based on what the human selected.
+        else {
+            switch(humanChoice.toUpperCase()){
+                case "ROCK":
+                    switch (computerChoice){
+                        case "PAPER":
+                            console.log("You lose! Paper beats Rock!");
+                            computerScore++;
+                            break;
+                        case "SCISSORS":
+                            console.log("You win! Rock beats Scissors!");
+                            humanScore++;
+                            break;
+                     }
+                break;
+                case "PAPER":
+                    switch (computerChoice){
+                        case "ROCK":
+                            console.log("You win! Paper beats Rock.");
+                            humanScore++
+                            break;
+                        case "SCISSORS":
+                            console.log("You lose! Scissors beats Paper.");
+                            computerScore++;
+                            break;
                 }
                 break;
-            case "PAPER":
-                switch (computerChoice){
-                    case 0:
-                        console.log("You win! Paper beats Rock.");
-                        humanScore++
-                        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
-                        break;
-                    case 1:
-                        console.log("Tie! You both picked Paper.");
-                        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
-                        break;
-                    default:
-                        console.log("You lose! Scissors beats Paper.");
-                        computerScore++;
-                        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
-                }
-                break;
-            default:
-                switch (computerChoice){
-                    case 0:
-                        console.log("You lose! Rock beats Scissors.");
-                        computerScore++;
-                        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
-                        break;
-                    case 1:
-                        console.log("You win! Scissors beats paper.");
-                        humanScore++;
-                        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
-                        break;
-                    default:
-                        console.log("Tie. You both picked Scissors.");
-                        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
-                }           
+                case "SCISSORS":
+                    switch (computerChoice){
+                        case "ROCK":
+                            console.log("You lose! Rock beats Scissors.");
+                            computerScore++;
+                            break;
+                        case "PAPER":
+                            console.log("You win! Scissors beats paper.");
+                            humanScore++;
+                            break;
+                } 
+                break;          
         }
     }
+    }
+    //Play Rock, Paper, Scissors 5 times against the computer.
     for (i=1; i<=5; i++){
         console.log(`Round ${i}`);
-        playRound(getHumanChoice(), getComputerChoice());
+        playRound(humanSelection(), computerSelection());
+        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
     }
+    console.log("Game Over. Thanks for playing!");
 }
-
 playGame();
