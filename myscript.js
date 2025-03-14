92c814c1d64254a0e5b360ca0dc3e1054cf22e74
Anthony Herrera <anthony.herrera@oracle.com>
2025-03-14 13:39:02 -0400

    Updated Rock Paper Scissors results on page.

--- myscript.js
+++ myscript.js
@@ -1,9 +1,9 @@
-
+//Based on exercise: https://www.theodinproject.com/lessons/foundations-revisiting-rock-paper-scissors
 
 //generate a number between 0 and 2
 //0 = Rock, 1= Paper, 2=Scissors
 //Return the string in UpperCase.
-computerSelection = function getComputerChoice(){
+function getComputerChoice(){
     switch(Math.floor((Math.random() * 3))){
         case 0:
             return "ROCK";
@@ -17,25 +17,17 @@ computerSelection = function getComputerChoice(){
     }
 }
 
-//Get Rock, Paper, or Scissor selection from Human. Return the value in Uppercase if the value is valid.
-humanSelection = function getHumanChoice(){
-    keepGoing = true;
-    while (keepGoing){
-        userInput = prompt("Enter the word Rock, Paper, or Scissors");
-        if (userInput !== null && (userInput.toUpperCase() == "ROCK" || userInput.toUpperCase() == "PAPER" || userInput.toUpperCase() == "SCISSORS")){
-            return userInput.toUpperCase();
-        }
-    }
-}
+let selectionResults = document.querySelector("#selectionResults");
+let scoreResults = document.querySelector("#scoreResults");
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice){
-        console.log(`You picked ${humanChoice} and the computer picked ${computerChoice}`);
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice){
+        computerChoice = getComputerChoice();
         //If its a tie, then skip all the checking to see who wins.
         if (humanChoice === computerChoice){
-            console.log("TIE!");
+            selectionResults.textContent = (`You picked ${humanChoice} and the computer picked ${computerChoice} - TIE!`);
         }
         //check to see who wins based on what the human selected.
         else {
@@ -43,11 +35,11 @@ function playGame(){
                 case "ROCK":
                     switch (computerChoice){
                         case "PAPER":
-                            console.log("You lose! Paper beats Rock!");
+                            selectionResults.textContent = (`You picked ${humanChoice} and the computer picked ${computerChoice} - You lose!`);
                             computerScore++;
                             break;
                         case "SCISSORS":
-                            console.log("You win! Rock beats Scissors!");
+                            selectionResults.textContent = (`You picked ${humanChoice} and the computer picked ${computerChoice} - You win!`);
                             humanScore++;
                             break;
                      }
@@ -55,11 +47,11 @@ function playGame(){
                 case "PAPER":
                     switch (computerChoice){
                         case "ROCK":
-                            console.log("You win! Paper beats Rock.");
+                            selectionResults.textContent = (`You picked ${humanChoice} and the computer picked ${computerChoice} - You win!`);
                             humanScore++
                             break;
                         case "SCISSORS":
-                            console.log("You lose! Scissors beats Paper.");
+                            selectionResults.textContent = (`You picked ${humanChoice} and the computer picked ${computerChoice} - You lose!`);
                             computerScore++;
                             break;
                 }
@@ -67,24 +59,27 @@ function playGame(){
                 case "SCISSORS":
                     switch (computerChoice){
                         case "ROCK":
-                            console.log("You lose! Rock beats Scissors.");
+                            selectionResults.textContent = (`You picked ${humanChoice} and the computer picked ${computerChoice} - You lose!`);
                             computerScore++;
                             break;
                         case "PAPER":
-                            console.log("You win! Scissors beats paper.");
+                            selectionResults.textContent = (`You picked ${humanChoice} and the computer picked ${computerChoice} - You win!`);
                             humanScore++;
                             break;
                 } 
                 break;          
         }
     }
+    scoreResults.textContent=(`Score is you: ${humanScore}, computer: ${computerScore}`);
+    if (humanScore == 5){
+        scoreResults.textContent=(`Final Score - you: ${humanScore}, computer: ${computerScore} You win!`);
+        humanScore = 0;
+        computerScore = 0;
     }
-    //Play Rock, Paper, Scissors 5 times against the computer.
-    for (i=1; i<=5; i++){
-        console.log(`Round ${i}`);
-        playRound(humanSelection(), computerSelection());
-        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+    else if (computerScore ==5){
+        scoreResults.textContent=(`Final Score -  you: ${humanScore}, computer: ${computerScore} You lose!`);
+        humanScore = 0;
+        computerScore = 0;
     }
-    console.log("Game Over. Thanks for playing!");
 }
-playGame();
+ 
